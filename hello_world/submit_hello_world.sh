835859988cfe92136a5bde6e08f61fd9e02945dd
Zach Merrill <zemerrill@gmail.com>
2021-01-28 16:42:45 -0500

    - != _

--- hello_world/submit_hello_world.sh
+++ hello_world/submit_hello_world.sh
@@ -4,4 +4,4 @@
 #SBATCH --ntasks-per-node=3
 #SBATCH --mem-per-cpu=32M       # memory; default unit is megabytes
 #SBATCH --time=0-00:05          # time (DD-HH:MM)
-srun ./hello-world              # mpirun or mpiexec also work
+srun ./hello_world              # mpirun or mpiexec also work
