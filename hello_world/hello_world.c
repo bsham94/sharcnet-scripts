953e9ef561e05d4f8f873a79b5a18ec88d15c614
Zach Merrill <zemerrill@gmail.com>
2021-01-28 16:03:20 -0500

    Learn to proofread before pressing commit

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
         printf("[ ERROR ]");
     }
 
-    MPI_Comm_size(MPI_COMM_WORLD), &num_procs);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &ID);
     printf("Hello world from process %d of %d \n", ID, num_procs);
 
