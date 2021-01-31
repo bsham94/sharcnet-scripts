3d56a9097f798dd5b0513b152198721758ad6dc5
Zach Merrill <zemerrill@gmail.com>
2021-01-31 13:49:31 -0500

    Illias' parallel_hello_world_with_messages

---
+++ hello_world/submit_hello_messages.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#SBATCH --account=def-ikotsire  # cp431g4 throws error
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=3
+#SBATCH --mem-per-cpu=32M       # memory; default unit is megabytes
+#SBATCH --time=0-00:05          # time (DD-HH:MM)
+srun ./hello_messages              # mpirun or mpiexec also work
