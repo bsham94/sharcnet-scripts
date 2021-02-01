55779ad497139187971df59e21f11a2d9d610157
Ben <ben.shamas@live.com>
2021-02-01 17:00:28 -0500

    stuff

--- a1/bprimes.c
+++ a1/bprimes.c
@@ -24,7 +24,9 @@ int main(int argc, char **argv)
     if(my_rank == 0 )
     {   
         int result = (n / (processors-1));
+        //Create array for all start numbers
         ranges = (unsigned long int*) malloc(sizeof(long long) * (processors - 1));
+        //Create Ranges
         for (int i = 0; i < (processors-1); i++)
         {
             int number = 0;
@@ -38,37 +40,47 @@ int main(int argc, char **argv)
             }
             ranges[i] = (i * result) + number;
         }
+        //Send range of numbers to all processes
         for (int i = 1; i < processors; i++)
         {
+            //Sends start and end range to all process but the last one
+            //Last one stops at n.
             if (i == (processors-1))
             {
                 MPI_Send(&(ranges[i - 1]), 1, MPI_UNSIGNED_LONG, i, tag, MPI_COMM_WORLD);
             }
             else
             {
+                //Start number
                 MPI_Send(&(ranges[i - 1]), 1, MPI_UNSIGNED_LONG, i, tag, MPI_COMM_WORLD);
+                //End number
                 MPI_Send(&(ranges[i]), 1, MPI_UNSIGNED_LONG, i, tag, MPI_COMM_WORLD);
-
             }                        
         }
         free(ranges);
     }
+    //The last process
     else if (my_rank == (processors -1))
     {
         unsigned long int start = 0;
+        //Recieve from process 0
         MPI_Recv(&start, 1, MPI_UNSIGNED_LONG, 0, tag, MPI_COMM_WORLD, &status);
         printf("Rank: %d\n", my_rank);
         printf("%lu\n", start);
         printf("%lu\n", n);
     }
+    //Every process but the last one.
     else
     {   
         unsigned long int start = 0;
         unsigned long int end = 0;
+        //Recieve from process 0
         MPI_Recv(&start, 1, MPI_UNSIGNED_LONG, 0, tag, MPI_COMM_WORLD, &status);
         MPI_Recv(&end, 1, MPI_UNSIGNED_LONG, 0, tag, MPI_COMM_WORLD, &status);
         printf("Rank: %d\n", my_rank);
         printf("%lu\n",start);
+        //end variable is the start of the next range of numbers
+        //-1 to stop before that numbers
         printf("%lu\n", end-1);
     }
     
