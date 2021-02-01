db2a1b27952c2c7ff9a094e8d9c7ce79f7669a69
Zach Merrill <zemerrill@gmail.com>
2021-02-01 12:24:27 -0500

    Init

---
+++ a1/submit_primes.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#SBATCH --account=def-ikotsire  # cp431g4 throws error
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=128M       # memory; default unit is megabytes
+#SBATCH --time=0-00:05          # time (DD-HH:MM)
+srun primes              # mpirun or mpiexec also work
