8279c09d5519cce40f1c9220ec7ff02a5e3f28c4
Zach Merrill <zemerrill@gmail.com>
2021-02-01 13:55:13 -0500

    trying less nodes

--- a1/submit_zprimes.sh
+++ a1/submit_zprimes.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --account=def-ikotsire  # cp431g4 throws error
-#SBATCH --nodes=8
-#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=4
 #SBATCH --mem-per-cpu=1024M       # memory; default unit is megabytes
 #SBATCH --time=0-00:05          # time (DD-HH:MM)
 srun zprimes              # mpirun or mpiexec also work
