f9a1a0753aa737a592d6016bcbb2e0a9a4b4f323
Zach Merrill <zemerrill@gmail.com>
2021-02-01 12:54:56 -0500

    made my own files for experimenting

---
+++ a1/zprimes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "mpi.h"
+#include "gmp.h"
+
+int main(int argc, char** argv) {
+    int myRank; // this rank
+    int numProcesses; // number of processes running
+    int source; // rank of the sender
+    int dest; // rank of the receiver
+    int tag = 0;
+    char message[100];
+    MPI_Status status; // return status for receive message
+
+    // Start MPI
+    if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
+        printf("[ ERROR ]");
+    }
+
+    // Get process rank
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    // Get number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &num_processes)
+
+    if (myRank == 0) {
+        // Main process
+        for (source = 1; source < numProcesses; source++){
+            MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
+            printf("%s\n",message);
+        }
+    } else {
+        sprintf(message, "Greetings from process %d!", myRank);
+        dest = 0;
+        MPI_Send(message, strlen(message)+1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+}
