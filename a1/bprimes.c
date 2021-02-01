db46e39847f1544b264b80d484f3c9baaf36c0d9
Ben <ben.shamas@live.com>
2021-02-01 13:10:25 -0500

    fixed typo

--- a1/bprimes.c
+++ a1/bprimes.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
     //Start mpi
     MPI_Init(&argc, &argv);
     //Find process rank
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     //Find out number of processes
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
     if(my_rank != 0 )
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
         for (source = 1; source < p; source++)
         {
             MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD,&status);
-            print("%s\n",message);
+            printf("%s\n",message);
         }
     }
     MPI_Finalize();
