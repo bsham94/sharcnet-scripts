cf493cef664d8bd9f8a81b26eb85883a77081212
Zach Merrill <zemerrill@gmail.com>
2021-02-01 13:14:12 -0500

    8 processors??

--- a1/submit_zprimes.sh
+++ a1/submit_zprimes.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #SBATCH --account=def-ikotsire  # cp431g4 throws error
-#SBATCH --nodes=2
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=1024M       # memory; default unit is megabytes
 #SBATCH --time=0-00:05          # time (DD-HH:MM)
