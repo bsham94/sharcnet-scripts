e18434b1a2593b691dd7a1ad33e389386631a492
Zach Merrill <zemerrill@gmail.com>
2021-02-03 16:09:23 -0500

    gonna take a while, give it 5 hours

--- a1/submit_primes.sh
+++ a1/submit_primes.sh
@@ -2,5 +2,5 @@
 #SBATCH --account=def-ikotsire  # cp431g4 throws error
 #SBATCH --ntasks=32     # number of processors + 1; rank=0 will do nothing
 #SBATCH --mem-per-cpu=1024M     # memory; default unit is megabytes
-#SBATCH --time=0-00:30          # time (DD-HH:MM)
+#SBATCH --time=0-05:00          # time (DD-HH:MM)
 srun primes                     # mpirun or mpiexec also work
