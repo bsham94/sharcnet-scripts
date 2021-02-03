856dce1bc5ad7b4246349eaab44b043fd6b9af84
Zach Merrill <zemerrill@gmail.com>
2021-02-03 10:45:20 -0800

    unsigned long long

--- a1/primes.c
+++ a1/primes.c
@@ -3,7 +3,7 @@
 #include <gmp.h>
 #include <mpi.h>
 
-int calculateRange(int rank, int n, int p) {
+unsigned long long calculateRange(int rank, int n, int p) {
     /* Essentially, pigeonhole principal
      * n => pigeons
      * p => pigeonholes
@@ -24,7 +24,7 @@ int calculateRange(int rank, int n, int p) {
 
 int main(int argc, char** argv)
 {
-    unsigned long int n = 1000000000;    // Number to count to
+    unsigned long int n = 1000000000000;    // Number to count to
     int my_rank;        // Rank of process
     int processors;     // Number of process
     int tag = 0;        // Message tag
@@ -45,7 +45,7 @@ int main(int argc, char** argv)
 
 
         // Wait for responses from all processors
-        unsigned long int first, second, gap, largestFirst, largestSecond, largestGap = 0;
+        unsigned long long first, second, gap, largestFirst, largestSecond, largestGap = 0;
         for(int source = 1; source < processors; source++){
             MPI_Recv(&first, 1, MPI_UNSIGNED_LONG, source, tag, MPI_COMM_WORLD, &status);
             MPI_Recv(&second, 1, MPI_UNSIGNED_LONG, source, tag, MPI_COMM_WORLD, &status);
@@ -64,10 +64,10 @@ int main(int argc, char** argv)
     else
     {
         // Calculate start
-        unsigned long int start = calculateRange(my_rank - 1, n, processors - 1);
+        unsigned long long start = calculateRange(my_rank - 1, n, processors - 1);
 
         // Calculate end
-        unsigned long int end = calculateRange(my_rank, n, processors - 1) - 1;
+        unsigned long long end = calculateRange(my_rank, n, processors - 1) - 1;
         if (my_rank == (processors - 1)) {
             // Final processor reaches to the end
             end++;
@@ -75,11 +75,14 @@ int main(int argc, char** argv)
 
         // Initialize all the mpz_ts
         mpz_t first, second, gap, largestFirst, largestSecond, largestGap, max;
-        mpz_init_set_ui(first, start);
-        mpz_init_set_ui(max, end);
+        //mpz_init_set_ui(first, start);
+        mpz_import(first, 1, -1, sizeof(unsigned long long), 0, 0, &start);
+        //mpz_init_set_ui(max, end);
+        mpz_import(max, 1, -1, sizeof(unsigned long long), 0, 0, &end);
         mpz_init(second);
         mpz_init(gap);
-        mpz_init_set_ui(largestGap, 0);
+        //mpz_init_set_ui(largestGap, 0);
+        mpz_import(largestGap, 1, -1, sizeof(unsigned long long), 0, 0, &0);
         mpz_init(largestFirst);
         mpz_init(largestSecond);
 
@@ -103,12 +106,23 @@ int main(int argc, char** argv)
         }
 
         // Send all three to rank=0
-        unsigned long resultFirst = mpz_get_ui(largestFirst);
+        /*
+        unsigned long long resultFirst = mpz_get_ui(largestFirst);
         MPI_Send(&resultFirst, 1, MPI_UNSIGNED_LONG, dest, tag, MPI_COMM_WORLD);
-        unsigned long resultSecond = mpz_get_ui(largestSecond);
+        unsigned long long resultSecond = mpz_get_ui(largestSecond);
         MPI_Send(&resultSecond, 1, MPI_UNSIGNED_LONG, dest, tag, MPI_COMM_WORLD);
-        unsigned long resultGap = mpz_get_ui(largestGap);
+        unsigned long long resultGap = mpz_get_ui(largestGap);
         MPI_Send(&resultGap, 1, MPI_UNSIGNED_LONG, dest, tag, MPI_COMM_WORLD);
+        */
+       unsigned long long resultFirst = 0;
+       mpz_export(&resultFirst, 0, -1, sizeof(unsigned long long), 0, 0, largestFirst);
+       MPI_Send(&resultFirst, 1, MPI_UNSIGNED_LONG_LONG, dest, tag, MPI_COMM_WORLD);
+       unsigned long long resultSecond = 0;
+       mpz_export(&resultSecond, 0, -1, sizeof(unsigned long long), 0, 0, largestSecond);
+       MPI_Send(&resultSecond, 1, MPI_UNSIGNED_LONG_LONG, dest, tag, MPI_COMM_WORLD);
+       unsigned long long resultGap = 0;
+       mpz_export(&resultGap, 0, -1, sizeof(unsigned long long), 0, 0, largestGap);
+       MPI_Send(&resultGap, 1, MPI_UNSIGNED_LONG_LONG, dest, tag, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
