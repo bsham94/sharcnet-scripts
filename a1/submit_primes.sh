4b703cf30993c80317671825bf1a35e1004727ae
Zach Merrill <zemerrill@gmail.com>
2021-02-03 09:54:16 -0800

    forgot to add 1 for rank=0 doing nothing

--- a1/submit_primes.sh
+++ a1/submit_primes.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #SBATCH --account=def-ikotsire  # cp431g4 throws error
-#SBATCH --nodes=8               # number of processors + 1; node 0 will do nothing
+#SBATCH --nodes=9               # number of processors + 1; node 0 will do nothing
 #SBATCH --ntasks-per-node=1     
 #SBATCH --mem-per-cpu=1024M     # memory; default unit is megabytes
 #SBATCH --time=0-00:30          # time (DD-HH:MM)
