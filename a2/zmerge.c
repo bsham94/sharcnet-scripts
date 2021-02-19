c8746effc1ac44fee3c75f60b83c91775df4d4b6
Zach Merrill <zemerrill@gmail.com>
2021-02-18 22:46:49 -0800

    free() for good measure

--- a2/zmerge.c
+++ a2/zmerge.c
@@ -148,6 +148,7 @@ int main(int argc, char **argv)
                 printf("%d ", merge[i]);
             }
             printf("]\n");
+            free(merge);
         }
         else // Slave processes
         {
