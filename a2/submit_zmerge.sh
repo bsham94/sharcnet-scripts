f8f5f480a3a7ad09c5322b3ffbfe7ea40e79def7
Zach Merrill <zemerrill@gmail.com>
2021-02-19 21:54:36 -0500

    sample python submit

--- a2/submit_zmerge.sh
+++ a2/submit_zmerge.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 #SBATCH --account=def-ikotsire  # cp431g4 throws error
-#SBATCH --ntasks=32             # number of processors + 1
+#SBATCH --ntasks=19             # number of processors + 1
 #SBATCH --mem-per-cpu=1024M     # memory; default unit is megabytes
 #SBATCH --time=0-0:30           # time (DD-HH:MM)
-srun zmerge                     # mpirun or mpiexec also work
+
+module load python mpi4py
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install 
+
+srun ./zmerge.py
