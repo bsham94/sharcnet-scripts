0b5459c18d1283754b42fb75cc85d3709bc91d07
Zach Merrill <zemerrill@gmail.com>
2021-02-19 22:18:06 -0500

    trying some things

--- a2/submit_zmerge.sh
+++ a2/submit_zmerge.sh
@@ -4,9 +4,11 @@
 #SBATCH --mem-per-cpu=1024M     # memory; default unit is megabytes
 #SBATCH --time=0-0:30           # time (DD-HH:MM)
 
-module load python mpi4py
+module load python/3.6 mpi4py
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
-pip install 
+pip install --no-index --upgrade pip
+
+pip install --no-index -r requirements.txt
 
 srun ./zmerge.py
