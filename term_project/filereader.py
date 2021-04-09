b4d73352a594162bb34d112493211e455a8b9460
Ben <ben.shamas@live.com>
2021-04-09 16:23:16 -0400

    stuff

--- term_project/filereader.py
+++ term_project/filereader.py
@@ -24,10 +24,12 @@ my_rank = comm.Get_rank()
 # Find number of processes
 p = comm.Get_size()
 
-n = 28143
+n = 15
+#n = 28143
 # n = 56
 
-file = open("ens.matrix", 'r')
+file = open("lecture.txt", 'r')
+#file = open("ens.matrix", 'r')
 # file = open("toy_example.txt", 'r')
 
 lines = file.readlines()
@@ -47,50 +49,40 @@ print(f'Rank: {my_rank} Lower: {lower} Upper: {upper}')
 dangle = float(1/n)
 a = 0.85
 ee = (1-a)*(dangle*1)
-value = dangle * a + ee
-array = np.full((n, n), value, float)
+print(ee)
+array = np.full((n, n), ee, float)
 for line in lines:
     l = line.split()
     if len(l) == 3:
         # if int(l[0]) < upper and int(l[0]) >= lower:
         if len(l[0]) != 0 and len(l[1]) != 0 and len(l[2]) != 0:
             if float(l[2]) == -1.0:
-                array[int(l[0])] = np.full((1, n), value, float)
+                array[int(l[0])] += dangle
             else:
                 array[int(l[0])][int(l[1])] = (float(l[2]) * a) + ee
 array = array.transpose()
-
+# print(array)
+#prev_r = np.full((n, 1), 0)
+r = np.zeros((n, 1))
 prev_r = np.full((n, 1), dangle, float)
-r = np.empty((n, 1))
 count = 0
-firstiteration = True
-while not(np.array_equal(prev_r, r)):
-    if not firstiteration:
-        prev_r = r.copy()
+while not(np.array_equal(prev_r, r)) and count < 34:
+    #prev_r = r.copy()
     for i in range(lower, upper):
         r[i] = np.dot(array[i], prev_r)
         # r = np.around(r.copy(), decimals=10)
     comm.Allgatherv(r[lower:upper], r)
-    r = np.around(r, decimals=3)
+    prev_r = np.around(r, decimals=10)
+    count = count + 1
+    if my_rank == 0:
+        print(r)
+        print(count)
 
+# if my_rank == 0:
+#     print(array)
+#     for i in r:
+#         print(i)
 
-if my_rank == 0:
-    for i in r:
-        print(i)
-
-# Build each process matrix from file and Calculate Google Matrix
-# G = aS + (1-a)(1/n(ee^t))
-# for line in lines:
-#     l = line.split()
-#     if len(l) == 3:
-#         if int(l[0]) < upper and int(l[0]) >= lower:
-#             if len(l[0]) != 0 and len(l[1]) != 0 and len(l[2]) != 0:
-#                 if float(l[2]) == -1.0:
-#                     value = dangle * a + ee
-#                     array[int(l[0])-lower] = np.full((1, n), value, float)
-#                 else:
-#                     array[int(l[0])-lower][int(l[1])] = (float(l[2]) * a) + ee
-# array = array.transpose()
 # # Ax = b
 # # Each process calculates (upper - lower) amount values of the b vector and broadcasts its to the other processes
 # # When calculations are complete, each process should stop and wait for others to complete.
