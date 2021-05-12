402cd48ac227c441d47da3bc8835058fc81a30bd
Zach Merrill <zemerrill@gmail.com>
2021-05-12 11:37:38 -0400

    Delete old references

--- a2/submit_merge.sh
+++ a2/submit_merge.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-#SBATCH --account=def-ikotsire  # cp431g4 throws error
-#SBATCH --ntasks=19             # number of processors + 1
-#SBATCH --mem-per-cpu=1024M     # memory; default unit is megabytes
-#SBATCH --time=0-0:30           # time (DD-HH:MM)
+#SBATCH --account=<ACCOUNT NAME HERE>
+#SBATCH --ntasks=8                  # number of processors
+#SBATCH --mem-per-cpu=1024M         # memory; default unit is megabytes
+#SBATCH --time=0-05:00              # time (DD-HH:MM)
 
 module load python/3.6 mpi4py
 virtualenv --no-download $SLURM_TMPDIR/env
