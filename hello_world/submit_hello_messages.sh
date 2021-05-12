50f6ae4b1c28048dcedd373c1c4c6cc094eff260
Zach Merrill <zemerrill@gmail.com>
2021-05-12 11:30:16 -0400

    Remove hello_world test project

--- hello_world/submit_hello_messages.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-#SBATCH --account=def-ikotsire  # cp431g4 throws error
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=3
-#SBATCH --mem-per-cpu=128M       # memory; default unit is megabytes
-#SBATCH --time=0-00:05          # time (DD-HH:MM)
-srun ./hello_messages              # mpirun or mpiexec also work
